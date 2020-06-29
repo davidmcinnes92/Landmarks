4599d0a35eec7d4b1f038e69e6eaed788c9f7289
David McInnes <davidmcinnes92@gmail.com>
2020-06-29 18:27:00 +0100

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by David Mcinnes on 29/06/2020.
+//  Copyright © 2020 David Mcinnes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
